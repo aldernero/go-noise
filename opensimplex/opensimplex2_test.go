7d551d0b2b999a51f742b14db674eadcf85c3d4c
Vernon Miller <vernon.w.miller@gmail.com>
2023-08-26 11:54:56 -0600

    added benchmark for opensimplex2

--- opensimplex/opensimplex2_test.go
+++ opensimplex/opensimplex2_test.go
@@ -2,6 +2,7 @@ package opensimplex
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
@@ -20,6 +21,22 @@ func TestNoise2D(t *testing.T) {
 			val := noise.Noise2D(point[0], point[1])
 			assert.GreaterOrEqual(t, val, -1.0)
 			assert.LessOrEqual(t, val, 1.0)
+		}
+	}
+}
+
+func BenchmarkOpenSimplex2_Noise2D(b *testing.B) {
+	randSeed := int64(42)
+	noiseSeed := int64(1337)
+	noise := NewOpenSimplex2(noiseSeed)
+	rng := rand.New(rand.NewSource(randSeed))
+	points := make([][]float64, 100)
+	for i := 0; i < len(points); i++ {
+		points[i] = []float64{1080 * rng.Float64(), 1080 * rng.Float64()}
+	}
+	for i := 0; i < b.N; i++ {
+		for _, point := range points {
+			noise.Noise2D(point[0], point[1])
 		}
 	}
 }
